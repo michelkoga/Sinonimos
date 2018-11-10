c6bda83ba9a1bd47f744accfce06c874243daf1b
Michel Koga <koga.michel@gmail.com>
2018-11-11 03:26:59 +0900

    Initial Commit

---
+++ Sinonimos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sinonimos
+//
+//  Created by 古賀ミッシェル on 2018/11/11.
+//  Copyright © 2018 古賀ミッシェル. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
